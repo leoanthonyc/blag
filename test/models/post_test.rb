fff5a8d88f2126cbe756a86b213cd6b85703d59b
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-16 19:03:46 +0800

    Validate Post#content, add Post tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'missing content' do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test 'missing user' do
+    post = Post.new(content: 'Hello')
+    assert_not post.save
+  end
+
+  test 'complete fields' do
+    post = Post.new(content: 'Hello', user: User.first)
+    assert post.save
+  end
 end
