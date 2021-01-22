28e940b2d3badd8b588c8620a49b8e4e68011594
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-22 18:32:47 +0800

    Add Comment model test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'missing post' do
+    comment = Comment.new(content: 'First!')
+    assert_not comment.save
+  end
+
+  test 'missing user' do
+    comment = Comment.new(content: 'Hello', post: Post.first)
+    assert_not comment.save
+  end
+
+  test 'complete fields' do
+    comment = Comment.new(content: 'Hello', post: Post.first, user: User.first)
+    assert comment.save
+  end
 end
