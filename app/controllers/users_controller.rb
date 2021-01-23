bb919502f3029e666e389392dd8a20cc1367bb90
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-23 12:20:24 +0800

    Allow user to edit profile
    * as of now, only the About field can be updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 # no-doc
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: :show
+  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = User.all
@@ -11,9 +11,25 @@ class UsersController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to current_user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:about)
   end
 end
