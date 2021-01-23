72e60c5418bb3c81ef2fc6ac12b1c3c81ad56f25
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-23 11:09:53 +0800

    Transfer User view from user_posts to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,17 @@
 # no-doc
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: :show
 
   def index
     @users = User.all
   end
 
-  def show
-    @posts = current_user.posts
-    render 'posts/index'
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
