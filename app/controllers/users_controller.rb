76d0b8fe26065bee844942878d5463649b00dd2e
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-23 11:01:19 +0800

    List all users in Users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show
     @posts = current_user.posts
     render 'posts/index'
