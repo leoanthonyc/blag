abb1db3031afa04e4aff5383dcc000ecec85ccbd
Leo Catane <leoanthony.catane@protonmail.com>
2021-01-28 18:19:48 +0800

    Add comment/post created_time_ago instance method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
+require 'action_view'
+
+# no-doc
 class Comment < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
   belongs_to :post
 
@@ -9,4 +14,8 @@ class Comment < ApplicationRecord
   after_create_commit { broadcast_append_to 'comments' }
   after_update_commit { broadcast_replace_to 'comments' }
   after_destroy_commit { broadcast_remove_to 'comments' }
+
+  def created_time_ago
+    time_ago_in_words(created_at)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+require 'action_view'
+
+# no-doc
 class Post < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   validates :content, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
 
   default_scope -> { order(created_at: :desc) }
+
+  def created_time_ago
+    time_ago_in_words(created_at)
+  end
 end
